bb5f3095ab04db7df9e1757166783c2b3c3ace18
daikingok <nagadai0920@gmail.com>
2022-07-06 21:41:27 +0900

    0706更新

---
+++ JavaSample/atCoder/abc226/A.java
@@ -0,0 +1,15 @@
+package abc226;
+
+import java.util.Scanner;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner sc = new Scanner(System.in);
+		double s = sc.nextDouble();
+		System.out.println((Math.round(s)));
+		
+	}
+
+}
